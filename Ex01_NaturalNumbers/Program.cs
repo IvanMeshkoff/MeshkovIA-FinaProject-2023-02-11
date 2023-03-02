b7af60af5950a264c04b6f2d8cf6dba5a2e520cd
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-03-02 20:25:02 +0300

    Example 01: Show natural numbers from 1 to N

---
+++ Ex01_NaturalNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+Console.Clear();
+
+void NaturalNumber(int n)
+{
+  if (n > 0)
+  {
+    for (int i = 1; i <= n; i++)
+    {
+      Console.Write(i + "  ");
+    }
+  }
+  else
+  {
+    Console.Write($"Введеное число {n} не является натуральным!");
+  }
+}
+
+Console.WriteLine("Введите натуральное число N: ");
+int Number = int.Parse(Console.ReadLine());
+NaturalNumber(Number);
