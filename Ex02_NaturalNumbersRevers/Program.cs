01359da704ad72fd681f5236b01c3a5f21f294c8
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-03-02 20:59:40 +0300

    Example 02: Show natural numbers from N to 1

---
+++ Ex02_NaturalNumbersRevers/Program.cs
@@ -0,0 +1,22 @@
+﻿// Показать натуральные числа от N до 1, N задано
+Console.Clear();
+
+void NaturalNumber(int n)
+{
+  if (n > 0)
+  {
+    Console.WriteLine($"Натуральные числа от {n} до 1:");
+    for (int i = 1; i <= n; i++)
+    {
+      Console.Write((n + 1) - i + "  ");
+    }
+  }
+  else
+  {
+    Console.Write($"Введеное число {n} не является натуральным!");
+  }
+}
+
+Console.WriteLine("Введите натуральное число N: ");
+int Number = int.Parse(Console.ReadLine());
+NaturalNumber(Number);
