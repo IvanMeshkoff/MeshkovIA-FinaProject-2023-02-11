1a24f2d09b3cdf8959358d448ca86f2ab0d40a18
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-03-04 13:42:35 +0300

    Example 06: Write a program to calculate the Ackermann function

---
+++ Ex06_AckermanFunctions/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу вычисления функции Аккермана
+Console.Clear();
+
+int Akk(int n,int m)
+{ 
+    if (n == 0) return m + 1;
+    if (n != 0 && m == 0) return Akk(n - 1, 1);
+    if (n > 0 && m > 0) return Akk(n - 1, Akk(n, m - 1));
+    return Akk(n,m);
+}
+
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine(Akk(num1,num2));
