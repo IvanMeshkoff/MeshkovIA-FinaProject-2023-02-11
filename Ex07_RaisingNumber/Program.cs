502a55c557b5cb996468b90314f91189f07b0b18
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-03-04 13:52:10 +0300

    Example 07: Write a program to convert a number A to an integer wall B

---
+++ Ex07_RaisingNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу возведения числа А в целую стень B
+Console.Clear();
+
+Double exponentiation(Double arg1, Double arg2)
+{
+    Double result = 0;
+    result = Math.Pow(arg1, arg2);
+    return result;
+}
+
+Console.Write("Введите число возмодимое в степень: ");
+Double num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите степень числа: ");
+Double num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Результат возведения в степень: "+ exponentiation(num1, num2));
