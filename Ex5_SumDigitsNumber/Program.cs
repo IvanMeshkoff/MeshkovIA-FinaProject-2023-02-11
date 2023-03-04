1303470fd674bdda2ea850e2ff443e4cdeb25a03
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-03-04 13:37:02 +0300

    Example 04: Find the sum of elements from M to N

---
+++ Ex5_SumDigitsNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти сумму цифр числа
+Console.Clear();
+
+int SumNum(int arg)
+{
+    if(arg<10) return arg;
+
+    int digit = arg %10;
+    int nextValue = arg/10;
+     return digit+SumNum(nextValue);
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(SumNum(number));
